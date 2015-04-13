d65aa63fa2c4854274e42f3b9ac2c6f191f0f7ae
Blake Loring <blake_l@parsed.uk>
2015-04-13 11:03:50 +0100

    Update parser.cpp

--- CoreASM/src/parser.cpp
+++ CoreASM/src/parser.cpp
@@ -9,10 +9,19 @@ Parser::~Parser() {}
 bool Parser::parseLabel(char const*& input, ByteBuffer& buffer) {}
 bool Parser::parseBlock(char const*& input, ByteBuffer& buffer) {}
 
-void Parser::parse(char const* input, ByteBuffer& buffer) {
-	Token next = Token(INVALID_TOKEN);
-	do {
-		next = _tokeniser.nextToken(input);
-		printf("Token %i (%s)\n", next.tokenId(), next.tokenString() ? next.tokenString() : "");
-	} while (next.tokenId() != INVALID_TOKEN && next.tokenId() != TOKEN_EOF);
+bool Parser::parse(char const* input, ByteBuffer& buffer) {
+	
+	parseBlock(input, buffer);
+	
+	Token next = _tokeniser.peekToken(input);
+	
+	if (_tokeniser.tokenId() == INVALID_TOKEN) {
+		return false;
+	}
+	
+	if (_tokeniser.tokenId() == TOKEN_EOF) {
+		return true;
+	}
+	
+	return parse(input, buffer);
 }
