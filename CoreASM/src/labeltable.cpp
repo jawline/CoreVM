e72e5c89d0ffce340fb036b4f7e6146d33bbf7b3
Blake Loring <blake_l@parsed.uk>
2015-04-13 16:40:39 +0100

    Update labeltable.cpp

--- CoreASM/src/labeltable.cpp
+++ CoreASM/src/labeltable.cpp
@@ -1,3 +1,12 @@
 #include "labeltable.h"
 
 using namespace Assembler;
+
+void LabelTable::setLabel(std::string const& labelName, size_t position) {
+  _currentLabels[labelName] = position;
+}
+bool LabelTable::getLabel(std::string const& labelName, size_t& position) {
+  if (_currentLabels.find(labelName) != _currentLabels.end()) {
+    position = _currentLabels[labelName];
+  }
+}
