dff47bd044d56c1f5eea16f69f63cc9de3e204bb
Blake Loring <blake_l@parsed.uk>
2015-04-15 13:29:34 +0100

    Implement getRegisterId

---
+++ CoreVM/src/vm/registers.cpp
@@ -0,0 +1,37 @@
+#include "registers.h"
+#include <string.h>
+
+using namespace VM;
+
+RegisterID RegisterUtils::getRegisterId(char const* name) {
+  switch (*name) {
+    case 'A':
+      return RegisterA;
+    case 'B':
+      return RegisterB;
+    case 'C':
+      return RegisterC;
+    case 'D':
+      return RegisterD;
+    case 'E':
+      return RegisterE;
+    case 'F':
+      return RegisterF;
+    case 'G':
+      return RegisterG;
+    case 'H':
+      return RegisterH;
+    case 'I':
+      return RegisterI;
+    case 'J':
+      return RegisterJ;
+    case 'K':
+      return RegisterK;
+    case 'L':
+      return RegisterL;
+    case 'M':
+      return RegisterM;
+    default:
+      return InvalidRegister;
+  }
+}
