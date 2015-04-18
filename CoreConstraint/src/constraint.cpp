7259f4ba2a2cf720b554ddfe4904bb4f9a324177
Blake Loring <blake_l@parsed.uk>
2015-04-18 17:26:57 +0100

    Implement Constraint

--- CoreConstraint/src/constraint.cpp
+++ CoreConstraint/src/constraint.cpp
@@ -2,5 +2,20 @@
 
 using namespace Constraints;
 
-Constraint::Constraint() {}
+Constraint::Constraint() {
+	_value = 0;
+}
+
+Constraint::Constraint(unsigned int value) {
+	_value = value;
+}
+
 Constraint::~Constraint() {}
+
+void Constraint::addItem(Variable const& var, double multiplier) {
+	_items.push_back(ConstraintItem(var, multiplier));
+}
+
+void Constraint::setResult(unsigned int endValue) {
+	_value = endValue;
+}
--- CoreConstraint/src/constraint.h
+++ CoreConstraint/src/constraint.h
@@ -1,13 +1,23 @@
 #ifndef _SOLVER_CONSTRAINT_DEF_H_
 #define _SOLVER_CONSTRAINT_DEF_H_
 #include "variable.h"
+#include <vector>
 
 namespace Constraints {
+	typedef std::pair<Variable,double> ConstraintItem;
+
 	class Constraint {
 	private:
+		std::vector<ConstraintItem> _items;
+		unsigned int _value;
+
 	public:
 		Constraint();
+		Constraint(unsigned int value);
 		~Constraint();
+
+		void addItem(Variable const& var, double multiplier);
+		void setResult(unsigned int endValue);
 	};
 };
 
