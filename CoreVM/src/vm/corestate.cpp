f61f94f22454af8a7ad409775d74c425bc678894
Blake Loring <blake_l@parsed.uk>
2015-04-22 15:44:55 +0100

    Update corestate.cpp

--- CoreVM/src/vm/corestate.cpp
+++ CoreVM/src/vm/corestate.cpp
@@ -2,6 +2,22 @@
 
 using namespace VM;
 
+CoreState::CoreState(uint8_t* data, unsigned int size) {
+	_registers = make_shared<uint32_t>(new uint32_t[NumRegisters]);
+	_data = make_shared<uint8_t>(data);
+	_dataSize = size;
+	_copyRegistersOnWrite = false;
+	_copyDataOnWrite = false;
+}
+
+CoreState::CoreState(CoreState const& existing) {
+	_data = existing._data;
+	_dataSize = existing._dataSize;
+	_registers = existing._registers;
+	_copyRegistersOnWrite = true;
+	_copyDataOnWrite = true;
+}
+
 void CoreState::copyRegisters() {
 	uint32_t* registers = new uint32_t[NumRegisters];
 	memcpy(registers, _registers, sizeof(uint32_t) * NumRegisters);
@@ -10,8 +26,8 @@ void CoreState::copyRegisters() {
 }
 
 void CoreState::copyData() {
-  uint8_t* data = new uint8_t[_dataSize];
-  memcpy(data, _data, _dataSize);
-  _data = make_shared<uint8_t>(data);
+	uint8_t* data = new uint8_t[_dataSize];
+	memcpy(data, _data, _dataSize);
+	_data = make_shared<uint8_t>(data);
 	_copyDataOnWrite = false;
 }
