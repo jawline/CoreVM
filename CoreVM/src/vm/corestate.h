6d889bf64148eed5a0b9c25cac2782d62ff1e938
Blake Loring <blake_l@parsed.uk>
2015-04-22 15:33:14 +0100

    Update corestate.h

--- CoreVM/src/vm/corestate.h
+++ CoreVM/src/vm/corestate.h
@@ -1,27 +1,30 @@
 #ifndef _CORE_STATE_DEF_H_
 #define _CORE_STATE_DEF_H_
 #include <cstdint>
+#include "registers.h"
 
 namespace VM {
   class CoreState {
   private:
+	/**
+	 * Current register data
+	 */
+	uint32_t* _registers;
+		
+	/**
+	 * RAM contents
+	 */
+	uint8_t* _data;
+	unsigned int _dataSize;
+	
+	/**
+	 * Should the state be copied on write
+	 */
+	bool _copyRegistersOnWrite;
+	bool _copyDataOnWrite;
 
-		/**
-		 * Current register data
-		 */
-		uint32_t* _registers;
-		
-		/**
-		 * RAM contents
-		 */
-		uint8_t* _data;
-		unsigned int _dataSize;
-		
-		/**
-		 * Should the state be copied on write
-		 */
-		bool _copyRegistersOnWrite;
-		bool _copyDataOnWrite;
+	void copyRegisters();
+	void copyData();
 
   public:
     CoreState();
@@ -30,12 +33,26 @@ namespace VM {
     
     void setData(uint8_t* data, unsigned int size);
 
-    inline uint32_t getRegister(unsigned int num) const {
-      return _registers[num];
+    inline uint32_t getRegister(uint8_t registerId) const {
+      return _registers[registerId];
     }
 
-    void setRegister(unsigned int num, uint32_t value) {
-      _registers[num] = value;
+    inline void setRegister(uint8_t registerId, uint32_t value) {
+    	if (_copyRegistersOnWrite) {
+    		copyRegisters();
+    	}
+    	_registers[registerId] = value;
+    }
+    
+    inline uint8_t getDataByte(size_t location) const {
+    	return _data[location];
+    }
+    
+    inline void setDataByte(size_t location, uint8_t val) const {
+    	if (_copyDataOnWrite) {
+    		copyData();
+    	}
+    	_data[location] = val;
     }
   };
 }
