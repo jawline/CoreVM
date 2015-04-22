5832353896ef3c0e4fbf0f1b4583f9c7328f9619
Blake Loring <blake_l@parsed.uk>
2015-04-22 15:14:31 +0100

    Add more state to CoreState

--- CoreVM/src/vm/corestate.h
+++ CoreVM/src/vm/corestate.h
@@ -1,10 +1,42 @@
 #ifndef _CORE_STATE_DEF_H_
 #define _CORE_STATE_DEF_H_
+#include <cstdint>
 
 namespace VM {
   class CoreState {
   private:
+
+		/**
+		 * Current register data
+		 */
+		uint32_t* _registers;
+		
+		/**
+		 * RAM contents
+		 */
+		uint8_t* _data;
+		unsigned int _dataSize;
+		
+		/**
+		 * Should the state be copied on write
+		 */
+		bool _copyRegistersOnWrite;
+		bool _copyDataOnWrite;
+
   public:
+    CoreState();
+    CoreState(uint8_t* data, unsigned int size);
+    CoreState(CoreState const& existing);
+    
+    void setData(uint8_t* data, unsigned int size);
+
+    inline uint32_t getRegister(unsigned int num) const {
+      return _registers[num];
+    }
+
+    void setRegister(unsigned int num, uint32_t value) {
+      _registers[num] = value;
+    }
   };
 }
 
