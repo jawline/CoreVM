ef25b736c28ec01907ebadfe4cc20837185fe8e4
Blake Loring <blake_l@parsed.uk>
2015-04-23 09:49:57 +0100

    Use .get in corestate

--- CoreVM/src/vm/corestate.cpp
+++ CoreVM/src/vm/corestate.cpp
@@ -20,14 +20,14 @@ CoreState::CoreState(CoreState const& existing) {
 
 void CoreState::copyRegisters() {
 	uint32_t* registers = new uint32_t[NumRegisters];
-	memcpy(registers, _registers, sizeof(uint32_t) * NumRegisters);
+	memcpy(registers, _registers.get(), sizeof(uint32_t) * NumRegisters);
 	_registers = make_shared<uint32_t>(registers);
 	_copyRegistersOnWrite = false;
 }
 
 void CoreState::copyData() {
 	uint8_t* data = new uint8_t[_dataSize];
-	memcpy(data, _data, _dataSize);
+	memcpy(data, _data.get(), _dataSize);
 	_data = make_shared<uint8_t>(data);
 	_copyDataOnWrite = false;
 }
