baf0a15a7c4ff289746ec23f7ad3646c1c09de04
Blake Loring <blake_l@parsed.uk>
2015-04-27 09:38:13 +0100

    Update labeltable.cpp

--- CoreASM/src/labeltable.cpp
+++ CoreASM/src/labeltable.cpp
@@ -7,10 +7,12 @@ void LabelTable::setLabel(string const& labelName, size_t position) {
   _currentLabels[labelName] = position;
 }
 
-bool LabelTable::getLabel(string const& labelName, size_t& position) {
+bool LabelTable::getLabel(string const& labelName, size_t& position) const {
+
   if (_currentLabels.find(labelName) != _currentLabels.end()) {
     position = _currentLabels[labelName];
     return true;
   }
+
   return false;
 }
