d99c7b4dfb13848e36819c0b6cd09cf3122bd648
Blake Loring <blake_l@parsed.uk>
2015-04-27 09:50:45 +0100

    Update registers.h

--- CoreVM/src/vm/registers.h
+++ CoreVM/src/vm/registers.h
@@ -25,11 +25,21 @@ namespace VM {
 		InvalidRegister
 	} RegisterID;
 
+	/**
+	 * Utility functions which allow for easier parsing and debugging output
+	 */
 	class RegisterUtils {
 	private:
 		static char const* mapping[];
 	public:
+		/**
+		 * Return a registers string name (PC, A, B, etc..) from its numeric id
+		 */
 		static char const* getRegisterName(unsigned int id);
+		
+		/**
+		 * Return a registers numeric ID (0, 1, 2, ...) from its string name
+		 */
 		static RegisterID getRegisterId(char const* name);
 	};
 }
