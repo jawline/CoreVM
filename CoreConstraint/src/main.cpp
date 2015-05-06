e8908bfcc1ffc5c2301897bcff408e2c24573594
Blake Loring <blake_l@parsed.uk>
2015-05-06 11:38:58 +0100

    Check solveTable result from main

--- CoreConstraint/src/main.cpp
+++ CoreConstraint/src/main.cpp
@@ -65,8 +65,12 @@ int main(int argc, char** argv) {
 	std::vector<int> artificialColumns;
 	p.toTable(table, artificialColumns);
 	table.print();
-	Simplex::Solver::solveTable(table, artificialColumns, res);
-	printf("%f\n", res.result);
+	
+	if (Simplex::Solver::solveTable(table, artificialColumns, res)) {
+		printf("%f\n", res.result);
+	} else {
+		printf("Cannot solve\n");
+	}
 
 	return -1;
 }
