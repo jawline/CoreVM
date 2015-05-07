6943f422dc54b6740ce1a729e63662e12e9c7c12
Blake Loring <blake_l@parsed.uk>
2015-05-07 15:36:51 +0100

    Update problem_constants.h

--- CoreConstraint/src/problem_constants.h
+++ CoreConstraint/src/problem_constants.h
@@ -4,7 +4,14 @@
 namespace Constraints {
 	class ProblemConstants {
 	public:
+		/**
+		 * Name to be given to the objective column in the table
+		 */
 		static char const* cObjectColumnName;
+		
+		/**
+		 * Name to be given to the results column in the table
+		 */
 		static char const* cResultColumnName;
 	};
 }
