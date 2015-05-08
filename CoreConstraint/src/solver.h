e0690cba702c4e88653d1641f334f77991038cfe
Blake Loring <blake_l@parsed.uk>
2015-05-08 13:43:04 +0100

    Define findBasicData

--- CoreConstraint/src/solver.h
+++ CoreConstraint/src/solver.h
@@ -23,6 +23,7 @@ namespace Simplex {
 		static void makeOtherRowsUnit(Table& instance, int baseRow, int col);
 		static void setupArtificialTable(Table& instance, Table& original, std::vector<int> const& artificialVariables);
 		static void restoreTable(Table& instance, Table& original);
+		static void findBasicData(Table& instance, int*& rowBasicData, double*& rowBasicSolution);
 
 	public:
 		/**
