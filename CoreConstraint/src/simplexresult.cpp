818d6bc3adb7249f987bf74c38c9933b4ddbfa7a
Blake Loring <blake_l@parsed.uk>
2015-05-12 15:20:19 +0100

    Implement SimplexResult

--- CoreConstraint/src/simplexresult.cpp
+++ CoreConstraint/src/simplexresult.cpp
@@ -3,10 +3,27 @@ using namespace Simplex;
 
 SimplexResult::SimplexResult() {}
 
-SimplexResult::SimplexResult(Table const& instance, int* rowBasis) {}
+SimplexResult::SimplexResult(Table const& instance, int* rowBasis) {
+  
+  //Results column result is always 0,0
+  _results[0] = instance.getField(0,0);
+  
+  //Map the results column to the result basis's
+  for (unsigned int i = 1; i < instance.getNumRows(); i++) {
+    _results[rowBasis[i]] = instance.getField(rowBasis[i], 0);
+  }
+}
 
 SimplexResult::~SimplexResult() {}
 
-double SimplexResult::getResult() const {}
+double SimplexResult::getResult() const {
+  return _results[0];
+}
 
-bool SimplexResult::getResult(int column, double& result) const {}
+bool SimplexResult::getResult(int column, double& result) const {
+  if (_results.find(column) != _results.end()) {
+    result = _results[column];
+    return true;
+  }
+  return false;
+}
