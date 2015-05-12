f0fd4c0ce122075acc6beb5142ec7c193c499e6f
Blake Loring <blake_l@parsed.uk>
2015-05-12 15:17:17 +0100

    Create skeleton implementation

--- CoreConstraint/src/simplexresult.cpp
+++ CoreConstraint/src/simplexresult.cpp
@@ -1,2 +1,12 @@
 #include "simplexresult.h"
+using namespace Simplex;
 
+SimplexResult::SimplexResult() {}
+
+SimplexResult::SimplexResult(Table const& instance, int* rowBasis) {}
+
+SimplexResult::~SimplexResult() {}
+
+double SimplexResult::getResult() const {}
+
+bool SimplexResult::getResult(int column, double& result) const {}
