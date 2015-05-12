6349c01ff628d0861a6f956210f452d2ff433977
Blake Loring <blake_l@parsed.uk>
2015-05-12 15:13:31 +0100

    Started building SimplexResult's structure

--- CoreConstraint/src/simplexresult.h
+++ CoreConstraint/src/simplexresult.h
@@ -1,12 +1,21 @@
 #ifndef _SIMPLEX_RESULT_DEF_H_
 #define _SIMPLEX_RESULT_DEF_H_
+#include <map>
 
 namespace Simplex {
     /**
-    * Structure to hold results of simplex solver run
-    */
-    struct SimplexResult {
-        double result;
+     * Structure to hold results of simplex solver run
+     */
+    class SimplexResult {
+    private:
+        std::map<int, double> _results;
+    public:
+        SimplexResult();
+        SimplexResult(Table const& instance, int* rowBasis);
+        ~SimplexResult();
+        
+        double getResult() const;
+        bool getResult(int column, double& result) const;
     };
 };
 
