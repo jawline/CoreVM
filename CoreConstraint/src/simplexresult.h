41db6287a1c1bce0f097e9120d104c107d9766c4
Blake Loring <blake_l@parsed.uk>
2015-05-12 11:12:18 +0100

    Create simplexresult.h

---
+++ CoreConstraint/src/simplexresult.h
@@ -0,0 +1,13 @@
+#ifndef _SIMPLEX_RESULT_DEF_H_
+#define _SIMPLEX_RESULT_DEF_H_
+
+namespace Simplex {
+    /**
+    * Structure to hold results of simplex solver run
+    */
+    struct SimplexResult {
+        double result;
+    };
+};
+
+#endif //_SIMPLEX_RESULT_DEF_H_
