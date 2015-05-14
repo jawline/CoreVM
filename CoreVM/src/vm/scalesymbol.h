ea3f1fe48900dc7383bf26f9b57f99fb983128fb
Blake Loring <blake_l@parsed.uk>
2015-05-14 15:16:40 +0100

    Create scalesymbol.h

---
+++ CoreVM/src/vm/scalesymbol.h
@@ -0,0 +1,16 @@
+#ifndef _SCALE_SYMBOL_DEF_H_
+#define _SCALE_SYMBOL_DEF_H_
+#include "compositesymbol.h"
+
+namespace VM {
+  class ScaleSymbol: public CompositeSymbol {
+  private:
+    CompositeSymbol* _symbol;
+    int _scalar;
+  public:
+    ScaleSymbol(CompositeSymbol* symbol, int scalar);
+    virtual ~ScaleSymbol();
+  };
+}
+
+#endif //_SCALE_SYMBOL_DEF_H_
