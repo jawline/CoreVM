9edad0a1d02995c60b930488e4915226ef8564ab
Blake Loring <blake_l@parsed.uk>
2015-05-14 15:23:05 +0100

    addsymbol.cpp

---
+++ CoreVM/src/vm/addsymbol.cpp
@@ -0,0 +1,13 @@
+#include "addsymbol.h"
+
+using namespace VM;
+
+AddSymbol::AddSymbol(CompositeSymbol* symbol1, CompositeSymbol* symbol2) {
+  _symbol1 = symbol1;
+  _symbol2 = symbol2;
+}
+
+AddSymbol::~AddSymbol() {
+  delete _symbol1;
+  delete _symbol2;
+}
