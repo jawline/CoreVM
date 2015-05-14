c1d6755e91a5b960766d2bc484ed1c9710e2765c
Blake Loring <blake_l@parsed.uk>
2015-05-14 10:34:56 +0100

    Update corestate.h

--- CoreVM/src/vm/corestate.h
+++ CoreVM/src/vm/corestate.h
@@ -9,44 +9,44 @@ using namespace std;
 
 namespace VM {
     class CoreState {
-    private:
-    
+        private:
+        
         /**
-         * Last symbol created
-         */
+        * Last symbol created
+        */
         static unsigned long _lastSymbol;
-
-    	/**
-    	 * Current register data
-    	 */
-    	RegisterState* _registers;
-    		
-    	/**
-    	 * RAM contents
-    	 */
-    	shared_ptr<uint8_t> _data;
-    	unsigned int _dataSize;
-    	
-    	/**
-    	 * Should the state be copied on write
-    	 */
-    	bool _copyDataOnWrite;
-
+        
         /**
-         * The current symbolic state
-         */
+        * Current register data
+        */
+        RegisterState* _registers;
+        
+        /**
+        * RAM contents
+        */
+        shared_ptr<uint8_t> _data;
+        unsigned int _dataSize;
+        
+        /**
+        * Should the state be copied on write
+        */
+        bool _copyDataOnWrite;
+        
+        /**
+        * The current symbolic state
+        */
         Constraints::Problem _symState;
-
-    	void copyData();
-
-    public:
-    	CoreState(uint8_t* data, unsigned int size);
+        
+        void copyData();
+        
+        public:
+        CoreState(uint8_t* data, unsigned int size);
         CoreState(CoreState* existing);
-      	CoreState(CoreState& existing);
+        CoreState(CoreState& existing);
         ~CoreState();
-
+        
         void makeSymbolic(uint8_t registerId);
-
+        
         Constraints::Problem* getProblem() {
             return &_symState;
         }
@@ -62,64 +62,64 @@ namespace VM {
         inline void setSymbolicMultiplier(uint8_t reg, int val) {
             _regsiters[registerId].symbolicMultiplier = val;
         }
-
+        
         inline Constraints::Variable getVariable(uint8_t registerId) {
             return _registers[registerId].variable;
         }
-
+        
         inline void moveRegister(uint8_t dstId, uint8_t srcId) {
             _registers[dstId] = _registers[srcId];
         }
-
-      	inline uint32_t getRegisterUInt(uint8_t registerId) const {
-        	return _registers[registerId].value;
-      	}
-
-      	inline void setRegisterUInt(uint8_t registerId, uint32_t value) {
-        	_registers[registerId].value = value;
+        
+        inline uint32_t getRegisterUInt(uint8_t registerId) const {
+            return _registers[registerId].value;
+        }
+        
+        inline void setRegisterUInt(uint8_t registerId, uint32_t value) {
+            _registers[registerId].value = value;
             _registers[registerId].symbolic = false;
         }
         
         inline int32_t getRegisterInt(int8_t registerId) const {
-          	return _registers[registerId].value;
+            return _registers[registerId].value;
         }
         
         inline void setRegisterInt(int8_t registerId, int32_t value) {
-        	_registers[registerId].value = value;
+            _registers[registerId].value = value;
             _registers[registerId].symbolic = false;
         }
         
-    	inline uint8_t getDataByte(size_t location) const {
-        	return _data.get()[location];
+        inline uint8_t getDataByte(size_t location) const {
+            return _data.get()[location];
         }
         
         inline void setDataByte(size_t location, uint8_t val) {
-        	if (_copyDataOnWrite) {
-        		copyData();
-        	}
-        	_data.get()[location] = val;
+            if (_copyDataOnWrite) {
+                copyData();
+            }
+            _data.get()[location] = val;
         }
         
         inline uint32_t getDataUInt(size_t location) const {
-        	return *((uint32_t*) &_data.get()[location]);
+            return *((uint32_t*) &_data.get()[location]);
         }
         
         inline void setDataUInt(size_t location, uint32_t val) {
-        	*((uint32_t*) &_data.get()[location]) = val;
+            *((uint32_t*) &_data.get()[location]) = val;
         }
         
         inline int32_t getDataInt(size_t location) const {
-        	return *((int32_t*) &_data.get()[location]);
-    	}
+            return *((int32_t*) &_data.get()[location]);
+        }
         
         inline void setDataInt(size_t location, int32_t val) {
-        	*((int32_t*) &_data.get()[location]) = val;
+            *((int32_t*) &_data.get()[location]) = val;
         }
-        	
+        
         RegisterState const* getRegisters() const;
         uint8_t const* getData() const;
         unsigned int getDataSize() const;
-  };
+    };
 }
 
 #endif //_CORE_STATE_DEF_H_
